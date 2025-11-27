420c8cc60cd4423018b194d553e7cd19d295bc90
MintyU <mintyu.dev@gmail.com>
2025-11-27 12:48:56 +0900

    스프링 시큐리티란?

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher; // deprecated
+import org.springframework.web.util.pattern.PathPattern;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/**")).permitAll())
+                .csrf((csrf) ->csrf
+                        .ignoringRequestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        )))
+        ;
+        return http.build();
+    }
+}
